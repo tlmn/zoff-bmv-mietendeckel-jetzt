3a5657063727be8de962954cab50a79669933b09
Tilman Miraß <>
2020-02-26 19:01:12 +0100

    add helmet meta data

--- src/components/helmet.jsx
+++ src/components/helmet.jsx
@@ -1,4 +1,39 @@
 import React from "react";
 import Helmet from "react-helmet";
 
-export default () => <Helmet />;
+export default () => (
+  <Helmet>
+    <meta charSet="utf-8" />
+    <link rel="canonical" href="https://www.mietendeckel.jetzt" />
+    <title>
+      Mietendeckel jetzt!
+    </title>
+    <meta
+      name="title"
+      content="Mietendeckel jetzt"
+    />
+    <meta
+      name="description"
+      content=""
+    />
+    <meta
+      name="keywords"
+      content=""
+    />
+    <meta property="og:title" content="" />
+    <meta
+      property="og:site_name"
+      content=""
+    />
+    <meta property="og:url" content="https://www.mietendeckelrechner.de" />
+    <meta
+      property="og:description"
+      content=""
+    />
+    <meta property="og:type" content="website" />
+    <meta
+      property="og:image"
+      content="https://www.mietendeckel.jetzt/images/mietendeckel-jetzt.jpg"
+    />
+  </Helmet>
+);
