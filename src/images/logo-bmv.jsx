4fb72b2806439e50d04a02da691562ef0d35ec4f
Tilman Miraß <>
2020-02-26 22:49:00 +0100

    correct logos

--- src/images/logo-bmv.jsx
+++ src/images/logo-bmv.jsx
@@ -1,20 +1,28 @@
 import React from "react";
 
 export default props => (
-  <svg {...props} viewBox="0 0 48 41" xmlns="http://www.w3.org/2000/svg">
-    <path
-      d="m24.132 32.385c6.8389 0 12.382-5.3625 12.382-11.978 0-6.6155-5.5435-11.978-12.382-11.978s-12.382 5.3625-12.382 11.978c0 6.6156 5.5436 11.978 12.382 11.978z"
-      fill="#FFE200"
-    />
-    <path
-      d="m35.341 3.4774h9.3101v33.857h-9.3101"
-      stroke="#C4C4C4"
-      stroke-width="6"
-    />
-    <path
-      d="M12.3081 3.47742H3V37.3343H12.3081"
-      stroke="#C4C4C4"
-      stroke-width="6"
-    />
+  <svg {...props} viewBox="0 0 48 41">
+    <g>
+      <path
+        d="m24.132 32.385c6.8389 0 12.382-5.3625 12.382-11.978 0-6.6155-5.5435-11.978-12.382-11.978s-12.382 5.3625-12.382 11.978c0 6.6156 5.5436 11.978 12.382 11.978z"
+        fill="#ffe200"
+      />
+      <path
+        d="m35.341 3.4774h9.3101v33.857h-9.3101"
+        fill="none"
+        stroke="#c4c4c4"
+        stroke-linecap="butt"
+        stroke-linejoin="miter"
+        stroke-width="6"
+      />
+      <path
+        d="m12.308 3.4774h-9.3081v33.857h9.3081"
+        fill="none"
+        stroke="#c4c4c4"
+        stroke-linecap="butt"
+        stroke-linejoin="miter"
+        stroke-width="6"
+      />
+    </g>
   </svg>
 );
