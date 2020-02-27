55eee5760f89d48353ccb48f816ce0f2417547c2
Tilman Miraß <>
2020-02-27 22:03:36 +0100

    remove auto play

--- src/components/player.jsx
+++ src/components/player.jsx
@@ -6,10 +6,10 @@ export default ({ videoURL, classNames }) => (
     <div className="wrapper__player">
       <ReactPlayer
         className="player"
-        playing
         url={videoURL}
         width="100%"
         height="100%"
+        controls
       />
     </div>
   </div>
