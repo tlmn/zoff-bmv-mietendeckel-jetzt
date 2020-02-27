f55c58ab08e93ad33eb8ee9348bf8a703724107c
Tilman Miraß <>
2020-02-27 08:24:16 +0100

    amooth scroll and navbar wrapper

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,46 +1,65 @@
-import React from "react";
+import React, { useState, useRef, useLayoutEffect } from "react";
 import LogoBMV from "../images/logo-bmv";
 import LogoFacebook from "../images/facebook";
 import LogoTwitter from "../images/twitter";
 import Scrollspy from "react-scrollspy";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
-export default () => (
-  <div className="navbar">
-    <LogoBMV width="40" />
-    <Scrollspy
-      items={[
-        "was-kann-mietendeckel",
-        "wie-kann-ich-deckeln",
-        "faq",
-        "nach-dem-mietendeckel"
-      ]}
-      currentClassName="is-current"
-      className="navbar__menu"
-    >
-      <li>
-        <a href="#was-kann-mietendeckel">Was ist der Mietendeckel?</a>
-      </li>
-      <li>
-        <a href="#wie-kann-ich-deckeln">Wie kann ich deckeln?</a>
-      </li>
-      <li>
-        <a href="#faq">FAQ</a>
-      </li>
-      <li>
-        <a href="#nach-dem-mietendeckel">Mietendeckel, und dann?</a>
-      </li>
-    </Scrollspy>
-    <ul className="navbar__socialmedia">
-      <li>
-        <a href="">
-          <LogoFacebook width="30" />
-        </a>
-      </li>
-      <li>
-        <a href="">
-          <LogoTwitter width="30" />
-        </a>
-      </li>
-    </ul>
-  </div>
-);
+
+export default () => {
+  const [menuHeight, setMenuHeight] = useState(0);
+  const refMenu = useRef(null);
+  const offset = 50;
+
+  useLayoutEffect(() => {
+    function FsetMenuHeight() {
+      setMenuHeight(refMenu.current.scrollHeight);
+    }
+    FsetMenuHeight();
+    window.addEventListener("resize", FsetMenuHeight);
+    return () => window.removeEventListener("resize", FsetMenuHeight);
+  }, []);
+  return (
+    <>
+      <div className="navbar" ref={refMenu}>
+        <LogoBMV width="40" />
+        <Scrollspy
+          items={[
+            "was-kann-mietendeckel",
+            "wie-kann-ich-deckeln",
+            "faq",
+            "nach-dem-mietendeckel"
+          ]}
+          currentClassName="is-current"
+          className="navbar__menu"
+        >
+          <li>
+            <AnchorLink offset={offset}href="#was-kann-mietendeckel">Was ist der Mietendeckel?</AnchorLink>
+          </li>
+          <li>
+            <AnchorLink offset={offset}href="#wie-kann-ich-deckeln">Wie kann ich deckeln?</AnchorLink>
+          </li>
+          <li>
+            <AnchorLink offset={offset}href="#faq">FAQ</AnchorLink>
+          </li>
+          <li>
+            <AnchorLink offset={offset}href="#nach-dem-mietendeckel">Mietendeckel, und dann?</AnchorLink>
+          </li>
+        </Scrollspy>
+        <ul className="navbar__socialmedia">
+          <li>
+            <a href="https://www.facebook.com">
+              <LogoFacebook width="30" />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.twitter.com">
+              <LogoTwitter width="30" />
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div style={{ height: menuHeight + 10 + "px" }} />
+    </>
+  );
+};
