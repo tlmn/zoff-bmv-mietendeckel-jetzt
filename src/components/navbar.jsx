031f3a11b190dc250a85dac5ac605ac963a22f04
Tilman Miraß <>
2020-02-29 11:31:47 +0100

    add mobile menu

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -25,73 +25,67 @@ const NavBar = ({ items, links }) => {
 
   return (
     <>
-      <div className="d-none d-sm-block">
-        <div className="navbar" ref={refMenu}>
-          <AnchorLink href="#top">
-            <LogoBMV width="40" />
-          </AnchorLink>
-          <Scrollspy
-            items={hrefs}
-            currentClassName="is-current"
-            className="navbar__menu"
-            offset={-menuHeight}
-          >
-            {items.map(item => (
-              <li>
-                <AnchorLink offset={offset} href={item.href}>
-                  {item.label}
-                </AnchorLink>
-              </li>
-            ))}
-          </Scrollspy>
-          <ul className="navbar__socialmedia">
-            <li>
-              <a href={links.facebook}>
-                <LogoFacebook width="30" />
-              </a>
-            </li>
-            <li>
-              <a href={links.twitter}>
-                <LogoTwitter width="30" />
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div style={{ height: `${menuHeight}px` }} />
-      </div>
-      <div className="d-block d-sm-none">
-        <div className="navbar">
+      <div className="navbar" ref={refMenu}>
+        <AnchorLink href="#top">
           <LogoBMV width="40" />
-          <button
-            class="hamburger hamburger--squeeze"
-            type="button"
-            aria-label="Menu"
-            aria-controls="navigation"
-            onClick={() => {
-              setShowMenu(showMenu === true ? false : true);
-            }}
-          >
-            <span class="hamburger-box">
-              <span class="hamburger-inner"></span>
-            </span>
-          </button>
-          <Scrollspy
-            items={hrefs}
-            currentClassName="is-current"
-            className="navbar__menu"
-            offset={-menuHeight}
-            style={{ display: showMenu === true ? "block" : "none" }}
-          >
-            {items.map(item => (
-              <li>
-                <AnchorLink offset={offset} href={item.href}>
-                  {item.label}
-                </AnchorLink>
-              </li>
-            ))}
-          </Scrollspy>
-        </div>
+        </AnchorLink>
+        <Scrollspy
+          items={hrefs}
+          currentClassName="is-current"
+          className="navbar__menu d-none d-md-block"
+          offset={-menuHeight}
+        >
+          {items.map(item => (
+            <li>
+              <AnchorLink offset={offset} href={item.href}>
+                {item.label}
+              </AnchorLink>
+            </li>
+          ))}
+        </Scrollspy>
+        <ul className="navbar__socialmedia d-none d-md-flex">
+          <li>
+            <a href={links.facebook}>
+              <LogoFacebook width="30" />
+            </a>
+          </li>
+          <li>
+            <a href={links.twitter}>
+              <LogoTwitter width="30" />
+            </a>
+          </li>
+        </ul>
+        <span className="d-block d-md-none">Mietendeckel, jetzt!</span>
+        <button
+          class="hamburger hamburger--squeeze d-flex d-md-none"
+          type="button"
+          aria-label="Menu"
+          aria-controls="navigation"
+          onClick={() => {
+            setShowMenu(showMenu === true ? false : true);
+          }}
+        >
+          <span class="hamburger-box">
+            <span class="hamburger-inner"></span>
+          </span>
+        </button>
+        <Scrollspy
+          items={hrefs}
+          currentClassName="is-current"
+          className="navbar__menu"
+          style={{ display: showMenu === true ? "block" : "none" }}
+          offset={-menuHeight}
+        >
+          {items.map(item => (
+            <li>
+              <AnchorLink offset={offset} href={item.href}>
+                {item.label}
+              </AnchorLink>
+            </li>
+          ))}
+        </Scrollspy>
       </div>
+      <div style={{ height: `${menuHeight}px` }} />
     </>
   );
 };
