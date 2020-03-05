3d8ac39d5a62418e469514d47d31613ea37e1590
Tilman Miraß <>
2020-03-05 08:08:18 +0100

    add imprint

--- src/pages/impressum.jsx
+++ src/pages/impressum.jsx
@@ -1,13 +1,162 @@
-import React from 'react';
-import Helmet from '../components/helmet';
+import React from "react";
+import Helmet from "../components/helmet";
 
 const IndexPage = () => (
   <>
     <Helmet />
     <div className="container mx-auto d-block pt-5 pb-5">
       <div className="row">
-        <div className="col">
-          <h1 className=" text-center">Impressum</h1>
+        <div className="col-12 offset-0 col-lg-8 offset-lg-2">
+          <h1 className="text-center">Impressum</h1>
+          <div>
+            <p>Angaben gemäß § 5 TMG:</p>
+            <p>
+              Berliner Mieterverein e.V.
+              <br />
+              Landesverband Berlin im Deutschen Mieterbund
+              <br />
+              Spichernstraße 1<br />
+              10777 Berlin
+            </p>
+            <h3>Vertreten durch:</h3>
+            <p>
+              den Vorstand des Berliner Mieterverein e.V.:
+              <br />
+              Vorsitzender: Dr. Rainer Tietzsch
+              <br />
+              Schatzmeisterin: Dr. Jutta Hartmann
+              <br />
+              Schriftführerin: Gundel Riebe
+            </p>
+            <p>Geschäftsführer: Reiner Wild</p>
+            <h3>Kontakt:</h3>
+            <p>
+              Telefon: 030-226 260
+              <br />
+              Telefax: 030-226 26 162
+              <br />
+              E-Mail:{" "}
+              <a
+                title="Kontakt"
+                href="http://www.berliner-mieterverein.de/kontakt.htm"
+              >
+                bmv@berliner-mieterverein.de
+              </a>
+            </p>
+            <h3>Registereintrag:</h3>
+            <p>
+              Eintragung im Vereinsregister.
+              <br />
+              Registergericht:Amtsgericht Berlin-Charlottenburg
+              <br />
+              Registernummer: VR 531 Nz
+            </p>
+            <h3>Umsatzsteuer-ID:</h3>
+            <p>
+              Umsatzsteuer-Identifikationsnummer gemäß §27 a Umsatzsteuergesetz:
+              <br />
+              DE 136724920
+            </p>
+            <h3>Angaben zur Berufshaftpflichtversicherung:</h3>
+            <p>
+              Name und Sitz der Gesellschaft:
+              <br />
+              Allianz Versicherungs AG,
+              <br />
+              Abt. Vermögensschaden, Postfach, 10900 Berlin
+              <br />
+              über Rahmenvertrag Verlags- und Verwaltungsgesellschaft
+              <br />
+              des Deutschen Mieterbundes,
+              <br />
+              Littenstraße 10, 10179 Berlin,
+            </p>
+            <p>Geltungsraum der Versicherung: Deutschland</p>
+            <h3>Verantwortlich für den Inhalt nach § 55 Abs. 2 RStV:</h3>
+            <p>Dr. Rainer Tietzsch</p>
+            <h3>Quellenangaben für die verwendeten Bilder und Grafiken:</h3>
+            <p>Bilder, Texte und Multimedia sind urheberrechtlich geschützt.</p>
+            <p>
+              <i>
+                Quelle:{" "}
+                <a
+                  href="http://www.e-recht24.de/impressum-generator.html"
+                  rel="nofollow"
+                >
+                  http://www.e-recht24.de
+                </a>
+              </i>
+            </p>
+            <h3>Disclaimer</h3>
+            <p>
+              <strong>Haftung für Inhalte</strong>
+            </p>
+            <p>
+              Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene
+              Inhalte auf diesen Seiten nach den allgemeinen Gesetzen
+              verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter
+              jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde
+              Informationen zu überwachen oder nach Umständen zu forschen, die
+              auf eine rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur
+              Entfernung oder Sperrung der Nutzung von Informationen nach den
+              allgemeinen Gesetzen bleiben hiervon unberührt. Eine
+              diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der
+              Kenntnis einer konkreten Rechtsverletzung möglich. Bei
+              Bekanntwerden von entsprechenden Rechtsverletzungen werden wir
+              diese Inhalte umgehend entfernen.
+            </p>
+            <p>
+              <strong>Haftung für Links</strong>
+            </p>
+            <p>
+              Unser Angebot enthält Links zu externen Webseiten Dritter, auf
+              deren Inhalte wir keinen Einfluss haben. Deshalb können wir für
+              diese fremden Inhalte auch keine Gewähr übernehmen. Für die
+              Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter
+              oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten
+              wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße
+              überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der
+              Verlinkung nicht erkennbar. Eine permanente inhaltliche Kontrolle
+              der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer
+              Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von
+              Rechtsverletzungen werden wir derartige Links umgehend entfernen.
+            </p>
+            <p>
+              <strong>Urheberrecht</strong>
+            </p>
+            <p>
+              Die durch die Seitenbetreiber erstellten Inhalte und Werke auf
+              diesen Seiten unterliegen dem deutschen Urheberrecht. Die
+              Vervielfältigung, Bearbeitung, Verbreitung und jede Art der
+              Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der
+              schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers.
+              Downloads und Kopien dieser Seite sind nur für den privaten, nicht
+              kommerziellen Gebrauch gestattet. Soweit die Inhalte auf dieser
+              Seite nicht vom Betreiber erstellt wurden, werden die
+              Urheberrechte Dritter beachtet. Insbesondere werden Inhalte
+              Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine
+              Urheberrechtsverletzung aufmerksam werden, bitten wir um einen
+              entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen
+              werden wir derartige Inhalte umgehend entfernen.
+            </p>
+            <p>
+              <i>
+                Quellverweis:{" "}
+                <a
+                  href="http://www.e-recht24.de/muster-disclaimer.html"
+                  rel="nofollow"
+                >
+                  eRecht24 Disclaimer
+                </a>
+              </i>
+              <br />
+              <span>
+                <span>Berliner Mieterverein</span>
+              </span>
+              <br />
+              <span>26.05.2017</span>
+            </p>
+          </div>
         </div>
       </div>
     </div>
