6beaff9308b0fef8de66b9a7b454896f9bc22332
Tilman Miraß <>
2020-03-09 15:29:51 +0100

    add dummy text

--- src/sections/4-warum-mietendeckel.jsx
+++ src/sections/4-warum-mietendeckel.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 const Page = () => (
   <>
@@ -14,56 +14,54 @@ const Page = () => (
         <div className="row">
           <div className="col-lg-8 offset-lg-2">
             <p className="teaser pb-5 pt-3">
-              Der Mietendeckel soll die Entwicklungen auf dem Berliner Mietmarkt
-              stoppen. Mit drei Maßnahmen soll das gelingen: Mietenstopp,
-              Obergrenzen bei Wiedervermietung und Mietabsenkung. Sie wirken bis
-              2025 und betreffen die Vielzahl der 1,5 Millionen Berliner
-              Haushalte. Einige Wohnungsarten wie beispielsweise so genannter
-              sozialer Wohnungsbau oder Neubau sind vom Mietendeckel
-              ausgenommen.
+              Minim voluptate proident minim dolore. Proident amet officia
+              incididunt in eu minim tempor amet excepteur ex. Incididunt culpa
+              ex sunt tempor dolore officia officia Lorem laboris incididunt
+              consectetur. Do pariatur do esse minim laborum excepteur anim. Est
+              dolor duis exercitation ipsum cupidatat exercitation. Dolor
+              pariatur eiusmod aliqua duis ex quis elit proident.
             </p>
           </div>
         </div>
         <div className="row">
           <div className="col-12 col-lg-4">
-            <h3 className="circle circle--yellow">Mietenstopp</h3>
-            <p className="font-weight-bold mt-2">seit 23. Februar 2020</p>
+            <h3 className="circle circle--yellow">Platzhalter</h3>
             <p>
-              Ob Staffelmiete oder einfache Erhöhung: Mieterhöhungen machen
-              Wohnen immer teurer. Bis 2025 werden in Berlin die Mieten dank
-              Mietenstopp eingefroren. 2022 können sie leicht angehoben werden.
-              Auch Modernisierungen können nur noch zu einem geringeren Teil auf
-              die Mieter*innen umgelegt werden: mit höchstens 1 Euro je
-              Quadratmeter. Endlich keine Angst mehr vor Miethöhungen!
+              Ex dolore nulla aliquip nostrud amet labore. Commodo qui aliqua
+              ipsum veniam voluptate aute. Magna laborum cupidatat dolor enim.
+              Ullamco nulla dolor labore amet cillum mollit laboris eu ad. Id
+              anim dolor non ullamco cupidatat ipsum velit. Ea consectetur
+              laboris ad reprehenderit est laborum cupidatat adipisicing veniam
+              veniam aliquip irure aliquip amet. Sint reprehenderit voluptate
+              excepteur nulla do sunt voluptate incididunt nulla et.
             </p>
           </div>
           <div className="col-12 col-lg-4">
-            <h3 className="circle circle--flamingo">Wiedervermietung</h3>
-            <p className="font-weight-bold mt-2">seit 23. Februar 2020</p>
+            <h3 className="circle circle--flamingo">Mieterhöhungen bei Modernisierung</h3>
             <p>
-              Bei Mieterwechsel drehen Vermieter*innen oftmals an der
-              Preisschraube. So steigen Mietpreise von Mieter*in zu Mieter*in
-              besonders stark. Das verhindert jetzt der Mietendeckel. Er legt
-              Obergrenzen für Mietpreise bei Wiedervermietungen fest. Diese
-              Obergrenzen unterscheiden sich nach Baujahr des Hauses und
-              Ausstattung. Vermieter*innen sind verpflichtet, dieser Obergrenzen
-              einzuhalten. Welche Höchstwerte genau gelten, verrät unser
-              Mietendeckelrechner.
+              Amet qui proident ipsum culpa est nulla sit consectetur duis
+              tempor veniam. Ut veniam consectetur ullamco voluptate enim elit
+              voluptate. Quis consequat anim minim pariatur minim consequat anim
+              irure quis deserunt tempor proident tempor. Ipsum aliquip nisi
+              esse anim anim dolor incididunt laboris consectetur magna esse
+              labore aliquip. Culpa cupidatat enim pariatur tempor aliqua dolore
+              proident est id nulla culpa ea dolor. Eu et culpa cupidatat id
+              ullamco minim aliqua quis.Deserunt irure sit duis eu excepteur
+              sunt eiusmod nulla amet laborum minim excepteur officia
+              adipisicing. Minim nostrud adipisicing ad nisi velit aliquip sit
+              duis.
             </p>
           </div>
           <div className="col-12 col-lg-4">
-            <h3 className="circle circle--blue">Mietabsenkung</h3>
-            <p className="font-weight-bold mt-2">ab 23. November 2020</p>
+            <h3 className="circle circle--blue">Hohe Mieten</h3>
             <p>
-              Wer in den letzten Jahren das Glück hatte eine Wohnung zu finden,
-              muss das häufig teuer bezahlen. Die gute Nachricht dank
-              Mietendeckel: Mieten, die mehr als 20 % über den Obergrenzen für
-              Wiedervermietung liegen, werden nicht nur eingefroren. Ab
-              23.11.2020 können sie abgesenkt werden! Wann genau eine überhöhte
-              Miete vorliegt, richtet sich nach Baujahr, Austattung und Wohnlage
-              des Hauses. Vermieter sind verpflichtet, ihre Mieter*innen zu
-              informieren. Ob Sie Ihre Miete absenken können und wie hoch eine
-              Absenkung wäre, berechnet unser Mietendeckelrechner.
+              Sint tempor consectetur voluptate labore quis nostrud ut proident
+              aliquip elit dolor velit fugiat pariatur. Nulla culpa exercitation
+              sint aliquip. Ea labore reprehenderit incididunt laborum.
+              Excepteur labore culpa eiusmod eu et nisi anim labore labore est
+              eu. Proident tempor cillum eu adipisicing occaecat pariatur
+              consectetur nostrud sit sunt labore officia. Anim non voluptate
+              proident laborum incididunt tempor enim non in.
             </p>
           </div>
         </div>
