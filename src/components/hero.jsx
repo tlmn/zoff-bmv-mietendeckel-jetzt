7a32b59835082576546ebafdb667280aa1aee1b4
Tilman Miraß <>
2020-03-10 16:08:44 +0100

    add moving bubbles

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -14,9 +14,20 @@ const Hero = ({ images }) => {
           className="hero__overlay"
         />
         <div
+          id="background-wrap svg-overlay"
           style={{ paddingTop: "55%", marginTop: "-55%" }}
-          className="svg-overlay svg-overlay--alt-werden"
-        />
+        >
+          <div className="bubble x1"></div>
+          <div className="bubble x2"></div>
+          <div className="bubble x3"></div>
+          <div className="bubble x4"></div>
+          <div className="bubble x5"></div>
+          <div className="bubble x6"></div>
+          <div className="bubble x7"></div>
+          <div className="bubble x8"></div>
+          <div className="bubble x9"></div>
+          <div className="bubble x10"></div>
+        </div>
       </div>
       <div className="circleTop circle circle--yellow">
         <h1>
