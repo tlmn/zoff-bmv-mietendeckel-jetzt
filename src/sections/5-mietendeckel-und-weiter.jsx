3e0de787c43088d99a11156f84e0fd9ae37399fe
Tilman Miraß <>
2020-03-11 13:06:54 +0100

    add texts

--- src/sections/5-mietendeckel-und-weiter.jsx
+++ src/sections/5-mietendeckel-und-weiter.jsx
@@ -15,48 +15,79 @@ const Page = () => (
         <div className="col-lg-8 offset-lg-2 offset-0 col-12">
           <p className="teaser pb-5 pt-3">
             Der Berliner Mietendeckel ist zunächst bis 2025 begrenzt. In dieser
-            Verschaufpause für die Mieter*innen ist es an der Berliner Politik,
-            nachhaltige Antworten auf die Wohnungsfrage zu finden. Wohnraum muss
-            dauerhaft bezahlbar und verfügbar bleiben. Darum ist der
-            Mietendeckel der Auftakt für eine Wende in der Wohnungspolitik, die
-            viel zu lange Interessen von Spekulant*innen vor Mieter*innen
-            gestellt hat.
+            Verschnaufpause für die Mieter*innen ist es an der Politik, weitere
+            und nachhaltige Lösungen für die Wohnungskrise zu finden. Wohnraum
+            muss dauerhaft bezahlbar sein und ausreichend zur Verfügung stehen.
+            Der Mietendeckel muss der Auftakt für eine Wende in der
+            Wohnungspolitik werden, die viel zu lange die Interessen der
+            Immobilienwirtschaft und von Spekulanten vor die Interessen der
+            Mieter*innen gestellt hat.
           </p>
         </div>
       </div>
       <div className="row">
         <div className="col-12 col-lg-4">
-          <h3 className="circle circle--yellow">Umwandlungsverbot in angespannten Wohnungsmärkten</h3>
+          <h3 className="circle circle--yellow">
+            Berlin braucht ein Umwandlungsverbot
+          </h3>
           <p>
-            Esse fugiat aliqua exercitation id veniam sit. Sint duis et
-            cupidatat consequat tempor nulla. Minim dolor ullamco tempor enim
-            voluptate Lorem. Cupidatat eu duis minim do voluptate fugiat
-            cupidatat sunt nostrud amet ipsum. Quis eiusmod deserunt amet
-            nostrud magna consectetur occaecat in ipsum cupidatat nisi. Nostrud
-            ea non aliquip laboris anim. Ad ullamco commodo ut cillum. Commodo
-            officia in eiusmod labore ex sunt non duis sit.
+            Von 2010 bis 2018 sind in Berlin fast 100.000 Mietwohnungen in
+            Eigentumswohnungen umgewandelt worden. Dadurch entstand massiver
+            Verdrängungsdruck, denn leere Wohnungen lassen sich deutlich teurer
+            verkaufen als von Mieter*innen bewohnte. Doch der Druck erzeugt auch
+            Widerstand - im Protest von hunderten Hausgemeinschaften, die ihre
+            Wohnungen nicht durch steigende Mieten oder zweifelhafte
+            Eigenbedarfskündigungen verlieren wollen. Das Land Berlin muss sich
+            im Bund für gesetzliche Regelungen zum Schutz vor Verdrängung
+            einsetzen, um den Menschen Angst und Sorge auf den ohnehin
+            angespannten Wohnungsmärkten der Städte und Ballungszentren zu
+            nehmen. Wir fordern ein Umwandlungsverbot sowie deutlich
+            eingeschränkte Eigenbedarfsgründe!
           </p>
         </div>
         <div className="col-12 col-lg-4">
-          <h3 className="circle circle--white">Gemeinwohlorientierter Neubau</h3>
+          <h3 className="circle circle--white">
+            Gemeinwohlorientierter und bedarfsgerechter Wohnungsneubau
+          </h3>
           <p>
-            Occaecat minim ex quis adipisicing consectetur deserunt aliqua ipsum
-            deserunt. Qui sunt veniam ex veniam. Veniam pariatur tempor dolor ad
-            ut est dolor quis commodo sit. Id sint fugiat laborum aliqua
-            voluptate laborum quis do eiusmod elit sint consequat. Ut qui minim
-            aliquip qui adipisicing ullamco cillum cillum culpa.
+            In den vergangenen 10 Jahren ist das Immobilienkapital privater
+            Investoren vorwiegend in den Ankauf von Wohnungsbeständen geflossen.
+            Neubauinvestments gingen vor allem in den Bau von luxuriösen Miet-
+            und Eigentumswohnungen, denn die Renditeerwartungen waren
+            maßgeblich. Die bundesdeutsche Politik hat dieser Entwicklung nicht
+            Einhalt geboten, die europäische Zinspolitik hat sie begünstigt. Von
+            2015 bis 2019 ist die Bevölkerung in Berlin um 160.000
+            Einwohner*innen gewachsen. Deshalb ist jetzt verstärkt der Bau
+            dauerhaft gebundener, preisgünstiger Wohnungen durch
+            gemeinwohlorientierte Unternehmen und Investoren notwendig.
+            Genossenschaften, gemeinwohlorientierte Stiftungen, Baugruppen,
+            sozialpflichtige private Investoren und kommunale Träger kommen
+            dafür in Frage. Berlin muss im Bauplanungsrecht stärker durchgreifen
+            und geeignete Fördermittel in größerem Umfang als bisher
+            bereitstellen. Und wir brauchen endlich eine andere Bodenpolitik,
+            die auch einen Bodenpreisdeckel beinhaltet.
           </p>
         </div>
         <div className="col-12 col-lg-4">
-          <h3 className="circle circle--blue">Sozialverträgliche energetische Modernisierung</h3>
+          <h3 className="circle circle--blue">
+            Sozialverträgliche energetische Modernisierung
+          </h3>
           <p>
-            Dolore eiusmod voluptate eiusmod aute ea. Labore pariatur ad velit
-            magna exercitation tempor officia ea. Ea aliquip veniam voluptate
-            consequat irure fugiat. Officia minim pariatur ut et sint mollit
-            nisi esse culpa velit aute ullamco ut fugiat. Cillum laboris eu
-            incididunt voluptate nisi consequat occaecat ipsum reprehenderit
-            consequat ullamco sit. Sunt qui consequat nisi ad adipisicing. Sit
-            et ex Lorem elit eiusmod qui.
+            Bundesregierung und Bundestag aber auch die Länder haben es
+            versäumt, den Zielkonflikt zwischen notwendigen Klimaschutz- und
+            Energiesparmaßnahmen in bestehenden Wohngebäuden und der
+            Zahlungsfähigkeit der Mieter*innen aufzulösen. Der Mietendeckel hat
+            - nach massiver Kritik an den jahrelang möglichen drastischen
+            Mietsteigerungen nach Modernisierung - durch eine maximale
+            Mietsteigerung von 1 Euro/qm im Monat eine weitgehende Garantie für
+            eine Sozialverträglichkeit energetischer Sanierungen geschaffen.
+            Doch damit ist der Klimaschutz noch keinen Schritt vorangekommen und
+            zukünftig zu erwartende Energiepreissteigerungen können in den
+            energetisch schlechten Gebäuden wiederum zu einer sozialen
+            Schieflage führen. Die neue CO2-Steuer wird den Energiepreisanstieg
+            befeuern. Es braucht daher endlich ein wirksames Klimaschutzgesetz,
+            mit dem die Gebäudeeigentümer verpflichtet werden, in Stufen die
+            jeweils schlechtesten Gebäude sinnvoll und effizient zu sanieren.
           </p>
         </div>
       </div>
