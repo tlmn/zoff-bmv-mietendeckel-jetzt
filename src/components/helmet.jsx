374bf60e43f1b71a31d89bfdd66f963e47829d9d
Tilman Miraß <>
2020-03-13 19:55:41 +0100

    set SEO data

--- src/components/helmet.jsx
+++ src/components/helmet.jsx
@@ -5,14 +5,26 @@ const Helmet = () => (
   <ReactHelmet>
     <meta charSet="utf-8" />
     <link rel="canonical" href="https://www.mietendeckel.jetzt" />
-    <title>Mietendeckel jetzt!</title>
-    <meta name="title" content="Mietendeckel jetzt" />
-    <meta name="description" content="" />
-    <meta name="keywords" content="" />
-    <meta property="og:title" content="Mietendeckel, jetzt! Checke ob und wie du deckelst." />
-    <meta property="og:site_name" content="Mietendeckel jetzt!" />
-    <meta property="og:url" content="https://www.mietendeckelrechner.de" />
-    <meta property="og:description" content="" />
+    <title>Mietendeckel, jetzt!</title>
+    <meta name="title" content="Mietendeckel, jetzt!" />
+    <meta
+      name="description"
+      content="Der lautstarke Protest gegen die Berliner Mietenkrise ist endlich in der Politik angekommen. Seit 23. Februar 2020 gilt in Berlin ein Mietendeckel. Er soll Schluss machen mit den Exzessen auf dem Berliner Wohnungsmarkt. Der Mietendeckel betrifft die meisten der 1,5 Millionen Berliner Haushalte. Checke, ob und wie du deckelst auf www.mietendeckel.jetzt"
+    />
+    <meta
+      name="keywords"
+      content="Mietendeckel, Mietenkrise, Berlin, Mietenstopp, Wiedervermietung, Mietabsenkung, Mietendeckel berechnen, Mietendeckelrechner, Berliner Mieterverein, weniger Miete, Mietendeckel erklärt, Was ist/bedeutet Mietendeckel, weniger Miete Berlin, bezahlbarer Wohnraum Berlin, Wie nutze ich Mietendeckel, Miete sparen"
+    />
+    <meta
+      property="og:title"
+      content="Mietendeckel, jetzt! Checke ob und wie du deckelst."
+    />
+    <meta property="og:site_name" content="Mietendeckel, jetzt!" />
+    <meta property="og:url" content="https://www.mietendeckel.jetzt" />
+    <meta
+      property="og:description"
+      content="Der lautstarke Protest gegen die Berliner Mietenkrise ist endlich in der Politik angekommen. Seit 23. Februar 2020 gilt in Berlin ein Mietendeckel. Er soll Schluss machen mit den Exzessen auf dem Berliner Wohnungsmarkt. Der Mietendeckel betrifft die meisten der 1,5 Millionen Berliner Haushalte. Checke, ob und wie du deckelst auf www.mietendeckel.jetzt"
+    />
     <meta property="og:type" content="website" />
     <meta
       property="og:image"
