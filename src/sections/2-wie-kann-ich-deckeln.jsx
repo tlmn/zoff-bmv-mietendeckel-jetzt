acb792817044ae7ed8b1d43b75e09e58776804ae
Tilman Miraß <>
2020-03-15 10:27:45 +0100

    set correct url

--- src/sections/2-wie-kann-ich-deckeln.jsx
+++ src/sections/2-wie-kann-ich-deckeln.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 
-const ORIGIN = "https://mdrbmv.netlify.com/";
+const ORIGIN = "https://mdrbmv.netlify.com";
 
 const Page = () => {
   const iframe = useRef();
