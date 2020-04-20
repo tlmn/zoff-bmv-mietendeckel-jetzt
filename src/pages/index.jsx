d523b30415e415ef90bfa97ca3237644f6b9a130
Tilman Miraß <>
2020-04-21 00:51:54 +0200

    disable tracker

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -81,7 +81,7 @@ const IndexPage = () => {
       <WasKommtNachDemMietendeckel />
       <Materialien />
       <Footer />
-      <CookieConsent
+      {/* <CookieConsent
         location="bottom"
         buttonText="Akzeptieren"
         enableDeclineButton={true}
@@ -107,7 +107,7 @@ const IndexPage = () => {
         und Facebook Pixel anonymisiert auszuwerten. Du akzeptierst unsere
         Cookies, wenn Du fortfährst diese Website zu nutzen.{" "}
         <Link to="/datenschutz">Erfahre mehr</Link>
-      </CookieConsent>
+      </CookieConsent> */}
     </>
   );
 };
