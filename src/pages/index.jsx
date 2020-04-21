ea4a01fc29b7f38b94af313b3c1c3cf4b7d93d69
Tilman Miraß <>
2020-04-21 08:10:33 +0200

    refactor tracking

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactGA from "react-ga";
 
 import Helmet from "../components/helmet";
 import CookieConsent from "react-cookie-consent";
@@ -17,19 +18,28 @@ import "../assets/stylesheets/application.scss";
 import { Link } from "gatsby";
 
 const IndexPage = () => {
+  const [FBPixelStatus, setFBPixelStatus] = useState(false);
+
   const links = {
     twitter: "https://twitter.com/bmieterverein",
     facebook: "https://de-de.facebook.com/BerlinerMieterverein/",
     instagram: "https://instagram.com/BerlinerMieterverein",
     bmv: "https://www.berliner-mieterverein.de/",
     datenschutz: "/datenschutz",
-    impressum: "/impressum"
+    impressum: "/impressum",
   };
 
   function optOut() {
     if (typeof window !== "undefined") {
       window["ga-disable-UA-159671521-1"] = true;
     }
+  }
+
+  function enableTracking() {
+    setFBPixelStatus(true);
+    ReactGA.set({ anonymizeIp: true });
+    ReactGA.initialize("UA-159671521-1");
+    ReactGA.pageview(window.location.pathname + window.location.search);
   }
 
   return (
@@ -41,36 +51,39 @@ const IndexPage = () => {
         items={[
           {
             label: "Was ist der Mietendeckel?",
-            href: "#was-ist-der-mietendeckel"
+            href: "#was-ist-der-mietendeckel",
           },
           { label: "Mietendeckelrechner", href: "#mietendeckelrechner" },
           { label: "FAQ Mietendeckel", href: "#faq" },
           { label: "Warum Mietendeckel?", href: "#warum-mietendeckel" },
-          { label: "Mietendeckel und weiter?", href: "#mietendeckel-und-weiter" }
+          {
+            label: "Mietendeckel und weiter?",
+            href: "#mietendeckel-und-weiter",
+          },
         ]}
       />
       <Hero
         images={[
           {
             src: "header-keine-angst-vor-mieterhoehungen.jpg",
-            srcMobile: "header-mobile-keine-angst-vor-mieterhoehungen.jpg"
+            srcMobile: "header-mobile-keine-angst-vor-mieterhoehungen.jpg",
           },
           {
             src: "header-alt-werden-im-eigenen-kiez.jpg",
-            srcMobile: "header-mobile-alt-werden-im-eigenen-kiez.jpg"
+            srcMobile: "header-mobile-alt-werden-im-eigenen-kiez.jpg",
           },
           {
             src: "header-platz-fuer-freunde.jpg",
-            srcMobile: "header-mobile-platz-fuer-freunde.jpg"
+            srcMobile: "header-mobile-platz-fuer-freunde.jpg",
           },
           {
             src: "header-alleine-wohnen.jpg",
-            srcMobile: "header-mobile-alleine-wohnen.jpg"
+            srcMobile: "header-mobile-alleine-wohnen.jpg",
           },
           {
             src: "header-reicht-die-kohle.jpg",
-            srcMobile: "header-mobile-reicht-die-kohle.jpg"
-          }
+            srcMobile: "header-mobile-reicht-die-kohle.jpg",
+          },
         ]}
       />
       <MietendeckelWirkt />
@@ -81,24 +94,32 @@ const IndexPage = () => {
       <WasKommtNachDemMietendeckel />
       <Materialien />
       <Footer />
-      {/* <CookieConsent
+      {FBPixelStatus === true && (
+        <img
+          height="1"
+          width="1"
+          src="https://www.facebook.com/tr?id=775019216286706&ev=PageView
+&noscript=1"
+        />
+      )}
+      <CookieConsent
         location="bottom"
         buttonText="Akzeptieren"
         enableDeclineButton={true}
         declineButtonText="Deaktivieren"
-        onDecline={() => optOut()}
         style={{
           background: "#ffe200",
           opacity: "0.9",
           alignItems: "center",
-          boxShadow: "0 2px 10px rgba(0,0,0,.15)"
+          boxShadow: "0 2px 10px rgba(0,0,0,.15)",
         }}
+        onAccept={() => enableTracking()}
         contentStyle={{ margin: "0.4rem" }}
         buttonStyle={{
           color: "#4e503b",
           fontSize: "1rem",
           margin: "0.4rem",
-          backgroundColor: "#fff"
+          backgroundColor: "#fff",
         }}
         declineButtonStyle={{ fontSize: "1rem", margin: "0.4rem" }}
         expires={0}
@@ -107,7 +128,7 @@ const IndexPage = () => {
         und Facebook Pixel anonymisiert auszuwerten. Du akzeptierst unsere
         Cookies, wenn Du fortfährst diese Website zu nutzen.{" "}
         <Link to="/datenschutz">Erfahre mehr</Link>
-      </CookieConsent> */}
+      </CookieConsent>
     </>
   );
 };
